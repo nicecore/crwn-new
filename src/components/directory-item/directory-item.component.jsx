ea2c6f337dd49782be3009349c92701030e5ddf3
Nicecore <adamdcameron@gmail.com>
2022-03-30 11:21:22 -0600

    deployed

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { BackgroundImage, DirectoryItemBody, DirectoryItemContainer } from "./directory-item.styles";
+import { useNavigate } from 'react-router-dom'
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage
         imageUrl={imageUrl}
       />
--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,15 +1,16 @@
-import './form-input.styles.scss'
+import {FormInputLabel, Input, Group} from './form-input.styles'
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div className="group">
-      <input className='form-input' {...otherProps} />
+    <Group>
+      <Input {...otherProps} />
       {label && (
-        <label 
+        <FormInputLabel
+          shrink={otherProps.value.length}
           className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>{label}
-        </label>
+        </FormInputLabel>
       )}
-    </div>
+    </Group>
   );
 };
 
